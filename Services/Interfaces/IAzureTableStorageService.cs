031b59922ab25284c9c7472f11941e2a1eefdb33
Seváztian Soffia Otárola <sevaztian_soffia_otarola@hotmail.com>
2025-10-15 21:17:31 +0100

    Added ICAO countries in Portuguese, cleanup

--- Services/Interfaces/IAzureTableStorageService.cs
+++ Services/Interfaces/IAzureTableStorageService.cs
@@ -12,8 +12,8 @@ namespace Services.Interfaces
         Task AddEntityAsync<T>(string tableName, T entity) where T : class, ITableEntity;
         Task UpdateEntityAsync<T>(string tableName, T entity, ETag etag) where T : class, ITableEntity;
         Task DeleteEntityAsync(string tableName, string partitionKey, string rowKey);
-        Task<T> GetEntityAsync<T>(string tableName, string partitionKey, string rowKey) where T : class, ITableEntity;
-        Task<IEnumerable<T>> QueryEntitiesAsync<T>(string tableName, string filter = null) where T : class, ITableEntity, new();
+        Task<T?> GetEntityAsync<T>(string tableName, string partitionKey, string rowKey) where T : class, ITableEntity;
+        Task<IEnumerable<T>> QueryEntitiesAsync<T>(string tableName, string? filter = null) where T : class, ITableEntity, new();
         Task<IEnumerable<T>> QueryByPartitionKeyAsync<T>(string tableName, string partitionKey) where T : class, ITableEntity, new();
         Task<IEnumerable<string>> GetTablesAsync();
         Task CreateTableAsync(string tableName);
