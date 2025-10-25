d96f405723297f694c67010cd87bf63f6d74f8c4
Seváztian Soffia Otárola <sevaztian_soffia_otarola@hotmail.com>
2025-10-25 01:46:49 +0100

    Scroll up when trying to save an entity that is not validated

--- Web/wwwroot/js/utilities.js
+++ Web/wwwroot/js/utilities.js
@@ -7,3 +7,22 @@ window.scrollToTop = () => {
 window.scrollToTopInstant = () => {
     window.scrollTo(0, 0);
 };
+
+// Scroll modal content to top
+window.scrollModalToTop = (modalId) => {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        const modalBody = modal.querySelector('.modal-body');
+        if (modalBody) {
+            modalBody.scrollTo({ top: 0, behavior: 'instant' });
+        }
+    }
+};
+
+// Scroll element to top by ID
+window.scrollElementToTop = (elementId) => {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.scrollTo({ top: 0, behavior: 'instant' });
+    }
+};
